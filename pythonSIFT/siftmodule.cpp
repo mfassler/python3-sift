e43055d31f09e15aeae085ee75d3ddfc418e5efe
Mark Fassler <mfassler@users.noreply.github.com>
2017-12-20 19:45:13 +0900

    woohoo!  Get the binary feature vectors is working!  :-)

--- pythonSIFT/siftmodule.cpp
+++ pythonSIFT/siftmodule.cpp
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+using std::vector;
+
 static PyObject *SiftError;
 
 SiftGPU _global_sift;
@@ -13,9 +15,10 @@ SiftGPU _global_sift;
 static PyObject * sift_init(PyObject *self, PyObject *args) {
 
 	const char * s_argv[] = {"-v", "1",  "-fo", "0", "-da", "-tc2", "7680", "-nomc"};
+	//const char * s_argv[] = {"-v", "1",  "-fo", "0", "-tc2", "7680", "-nomc"};
 	int s_argc = sizeof(s_argv)/sizeof(char*);
 
-	printf("Hello from sift_create!\n");
+	//printf("Hello from sift_init!\n");
 
 	_global_sift.ParseParam(s_argc, (char **)s_argv);
 
@@ -26,7 +29,7 @@ static PyObject * sift_init(PyObject *self, PyObject *args) {
 		return PyLong_FromLong(-1);
 	}
 
-	return PyLong_FromLong(123456);
+	return Py_None;
 }
 
 
@@ -44,7 +47,7 @@ static PyObject * sift_RunSIFT(PyObject *self, PyObject *args) {
 		return NULL;
 	}
 
-	return PyLong_FromLong(retval);
+	return Py_None;
 }
 
 
@@ -57,14 +60,75 @@ static PyObject * sift_SaveSIFT(PyObject *self, PyObject *args) {
 
 	_global_sift.SaveSIFT(filename);
 
-	return PyLong_FromLong(0);
+	return Py_None;
+}
+
+
+
+static PyObject * sift_GetFeatureVector(PyObject *self, PyObject *args) {
+
+	unsigned int i, j;
+	unsigned int offset;
+
+	double value;
+
+	int numFeatures;
+	vector<float> descriptors(1);
+	// reminder:
+	// a SiftKeypoint is:  struct {float x,y,s,o;};
+	//    for x, y, scale, orientation
+	vector<SiftGPU::SiftKeypoint> keys(1);
+
+	printf("sizeof(float): %lu\n", sizeof(float));
+
+	numFeatures = _global_sift.GetFeatureNum();
+	printf("numFeatures: %d\n", numFeatures);
+
+
+	// Allocate memory
+	keys.resize(numFeatures);
+	descriptors.resize(128 * numFeatures);
+
+	_global_sift.GetFeatureVector(&keys[0], &descriptors[0]);
+
+	// Convert keys and descriptors into something useful we can give back to Python
+	PyObject* py_keyList = PyList_New(numFeatures * 4);
+	if (!py_keyList) {
+		PyErr_SetString(SiftError, "Failed to allocate memory for Python list");
+		return NULL;
+	}
+
+	PyObject* py_descList = PyList_New(numFeatures * 128);
+	if (!py_descList) {
+		PyErr_SetString(SiftError, "Failed to allocate memory for Python list");
+		return NULL;
+	}
+
+
+	for (i = 0; i<numFeatures; ++i) {
+		offset = 4 * i;
+		PyList_SET_ITEM(py_keyList, offset, PyFloat_FromDouble((double) keys[i].x));
+		PyList_SET_ITEM(py_keyList, offset+1, PyFloat_FromDouble((double) keys[i].y));
+		PyList_SET_ITEM(py_keyList, offset+2, PyFloat_FromDouble((double) keys[i].s));
+		PyList_SET_ITEM(py_keyList, offset+3, PyFloat_FromDouble((double) keys[i].o));
+
+		offset = 128 * i;
+		for (j=0; j<128; ++j) {
+			value = (double) (descriptors[i*128 + j] * 512.0 + 0.5);
+
+			PyList_SET_ITEM(py_descList, offset+j, PyFloat_FromDouble(value));
+		}
+	}
+
+	return Py_BuildValue("OO", py_keyList, py_descList);
 }
 
 
 static PyMethodDef SiftMethods[] = {
-	{"create", sift_create, METH_VARARGS, "Create a SiftGPU instance"},
+	{"init", sift_init, METH_VARARGS, "Create a SiftGPU instance"},
 	{"RunSIFT", sift_RunSIFT, METH_VARARGS, "Run the SIFT feature detector"},
-	{"SaveSIFT", sift_SaveSIFT, METH_VARARGS, "Save the SIFT features"},
+	{"SaveSIFT", sift_SaveSIFT, METH_VARARGS, "Save the SIFT features as an ascii file"},
+	{"GetFeatureVector", sift_GetFeatureVector, METH_VARARGS, "Get the SIFT features as binary vector"},
 	{NULL, NULL, 0, NULL}
 };
 
---
+++ pythonSIFT/testPython.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import sys
+if sys.version_info[0] < 3:
+    raise Exception("Must be using Python 3")
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+goodDesc = np.array([
+    29, 1, 0, 5, 159, 2, 0, 0, 169, 5, 0, 0, 12, 1, 0, 8, 72, 1, 0, 0,
+    22, 7, 0, 4, 0, 0, 0, 1, 15, 4, 0, 0, 36, 0, 0, 1, 169, 14, 0, 0,
+    169, 4, 0, 0, 18, 1, 0, 3, 98, 1, 0, 2, 29, 4, 0, 1, 0, 0, 0, 1,
+    17, 10, 0, 0, 32, 0, 0, 0, 169, 18, 0, 2, 169, 2, 0, 0, 19, 2, 0, 9,
+    97, 1, 0, 3, 13, 12, 2, 3, 0, 0, 0, 1, 13, 15, 1, 0, 27, 1, 0, 6,
+    165, 3, 0, 1, 169, 3, 0, 1, 15, 1, 0, 6, 73, 1, 1, 6, 8, 5, 0, 3,
+    0, 0, 0, 2, 11, 7, 0, 0])
+
+
+lastGoodDesc = np.array([
+    3, 3, 4, 4, 0, 0, 1, 1, 14, 44, 77, 41, 1, 1, 1, 2, 10, 35, 141, 23,
+    5, 13, 10, 9, 0, 1, 52, 41, 8, 16, 4, 0, 4, 14, 43, 15, 1, 1, 1, 1,
+    61, 48, 136, 146, 21, 1, 1, 5, 146, 92, 75, 36, 26, 114, 66, 84, 15, 9, 39, 56,
+    108, 146, 34, 11, 5, 4, 3, 4, 2, 1, 0, 2, 82, 12, 8, 52, 34, 0, 0, 38,
+    146, 62, 41, 73, 15, 10, 2, 75, 13, 14, 42, 121, 67, 68, 2, 5, 0, 0, 0, 0,
+    0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 2, 10, 0, 1, 12, 1, 0, 0, 6,
+    1, 0, 2, 12, 1, 0, 0, 2])
+
+
+import sift
+
+sift.init()
+sift.RunSIFT('DJI_0085.JPG')
+
+keys, descriptors = sift.GetFeatureVector()
+
+numKeys = len(keys) / 4.0
+numDescs = len(descriptors) / 128.0
+
+assert numKeys == numDescs
+numFeatures = int(numKeys)
+
+keys = np.array(keys).reshape(numFeatures, 4)
+descriptors = np.array(descriptors, np.uint8).reshape(numFeatures, 128)
+
+'''
+loc = np.array(stuff[0:4])
+desc = np.array(stuff[4:132])
+
+lastLoc = np.array(stuff[-132:-128])
+lastDesc = np.array(stuff[-128:])
+'''
